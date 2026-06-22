73e52dcb955b68ab45b1625817946f6b867e66e7
Rafael Arantes <67936069+rafaelangeloarantes@users.noreply.github.com>
2026-06-22 13:16:06 -0300

    Delete frontend directory

--- frontend/src/pages/PesquisasPage.jsx
+++
@@ -1,9 +0,0 @@
-import './PesquisasPage.css'
-
-export default function PesquisasPage() {
-  return (
-    <div className="pesquisas-page">
-      <p className="pesquisas-page__text">Módulo de pesquisas em construção.</p>
-    </div>
-  )
-}
